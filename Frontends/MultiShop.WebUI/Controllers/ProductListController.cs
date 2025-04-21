fde8b3ee6f079821872c558baaea2d77ba2c9a99
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-21 21:37:27 +0300

    productlist design and view components

---
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ProductListController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürünler";
+            ViewBag.directory3 = "Ürün Listesi";
+            return View();
+        }
+    }
+}
