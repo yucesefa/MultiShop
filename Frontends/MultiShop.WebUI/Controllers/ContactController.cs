619feb24120cd938cffafb9da90cb5844c19b172
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-21 22:25:40 +0300

    shopping cart & contact design with view component

---
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.directory1 = "MultiShop";
+            ViewBag.directory2 = "İletişim";
+            ViewBag.directory3 = "Mesaj Gönder";
+            return View();
+        }
+    }
+}
