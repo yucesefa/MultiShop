c0cd640308572be80807228be513d6dad8dc6c1b
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-21 22:00:29 +0300

    product detail design with view components

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -5,11 +5,20 @@ namespace MultiShop.WebUI.Controllers
     public class ProductListController : Controller
     {
 
-        public IActionResult Index()
+        public IActionResult Index(/*string id*/)
         {
             ViewBag.directory1 = "Ana Sayfa";
             ViewBag.directory2 = "Ürünler";
             ViewBag.directory3 = "Ürün Listesi";
+            //ViewBag.i = id;
+            return View();
+        }
+        public IActionResult ProductDetail(/*string id*/)
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürün Listesi";
+            ViewBag.directory3 = "Ürün Detayları";
+            //ViewBag.x = id;
             return View();
         }
     }
