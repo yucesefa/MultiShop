f8bd471ac791931cd9b7d75a434b91c75f95c23d
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-29 00:08:30 +0300

    brand api and consume operations

--- Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.BrandDtos;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Dtos.FeatureDtos;
 using MultiShop.Catalog.Dtos.FeatureSliderDtos;
@@ -56,6 +57,11 @@ namespace MultiShop.Catalog.Mapping
             CreateMap<OfferDiscount, CreateOfferDiscountDto>().ReverseMap();
             CreateMap<OfferDiscount, UpdateOfferDiscountDto>().ReverseMap();
             CreateMap<OfferDiscount, GetByIdOfferDiscountDto>().ReverseMap();
+
+            CreateMap<Brand, ResultBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandDto>().ReverseMap();
+            CreateMap<Brand, UpdateBrandDto>().ReverseMap();
+            CreateMap<Brand, GetByIdBrandDto>().ReverseMap();
 
 
         }
