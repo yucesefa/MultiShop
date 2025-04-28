5b73ccf25ef6f2b680495c6cf84f5754e4a0c165
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-04-28 20:44:05 +0300

    feature api and consume operations

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeaturesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.FeatureDtos;
+using MultiShop.Catalog.Services.FeatureServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IFeatureService _categoryService;
+
+        public FeaturesController(IFeatureService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FeatureList()
+        {
+            var values = await _categoryService.GetAllFeatureAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeatureById(string id)
+        {
+            var value = await _categoryService.GetByIdFeatureAsync(id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            await _categoryService.CreateFeatureAsync(createFeatureDto);
+            return Ok("Bilgi Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFeature(string id)
+        {
+            await _categoryService.DeleteFeatureAsync(id);
+            return Ok("Bilgi Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            await _categoryService.UpdateFeatureAsync(updateFeatureDto);
+            return Ok("Bilgi Güncellendi");
+        }
+    }
+}
