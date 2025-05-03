5e9d997fddf182684f20d483a755d45e0c34cb47
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-05-03 22:10:06 +0300

    contact api & consume operations

--- Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,14 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using Newtonsoft.Json;
+using System.Text;
+
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.directory1 = "MultiShop";
             ViewBag.directory2 = "İletişim";
             ViewBag.directory3 = "Mesaj Gönder";
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            createContactDto.IsRead = false;
+            createContactDto.SendDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7070/api/Contacts", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
