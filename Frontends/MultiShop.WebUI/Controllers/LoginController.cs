7b9c9ba71a7dea135653bb1cd4cb1f91831b7cc2
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-05-03 23:05:09 +0300

    login api operations with identity server

---
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.DtoLayer.IdentityDtos.RegisterDtos;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(SignInDto signInDto)
+        {
+           
+            return View();
+        }
+    }
+}
--- IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -9,7 +9,7 @@ using static IdentityServer4.IdentityServerConstants;
 
 namespace MultiShop.IdentityServer.Controllers
 {
-    [Authorize(LocalApi.PolicyName)] // identity server access token a sahip olmak zorunda
+    ////[Authorize(LocalApi.PolicyName)] // identity server access token a sahip olmak zorunda
     [Route("api/[controller]")]
     [ApiController]
     public class RegistersController : ControllerBase
