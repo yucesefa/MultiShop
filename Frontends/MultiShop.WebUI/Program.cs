e4d22b1b91a139ecd1bd0157203d1ffdc70d9d5e
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-06-29 16:43:49 +0300

    transformations for product detail services

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -7,6 +7,8 @@ using MultiShop.WebUI.Services.CatalogServices.CategoryServices;
 using MultiShop.WebUI.Services.CatalogServices.FeatureServices;
 using MultiShop.WebUI.Services.CatalogServices.FeatureSliderServices;
 using MultiShop.WebUI.Services.CatalogServices.OfferDiscountServices;
+using MultiShop.WebUI.Services.CatalogServices.ProductDetailServices;
+using MultiShop.WebUI.Services.CatalogServices.ProductImageServices;
 using MultiShop.WebUI.Services.CatalogServices.ProductServices;
 using MultiShop.WebUI.Services.CatalogServices.SpecialOfferServices;
 using MultiShop.WebUI.Services.Concrete;
@@ -105,6 +107,17 @@ builder.Services.AddHttpClient<IAboutService, AboutService>(opt =>
 {
     opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
 }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
+builder.Services.AddHttpClient<IProductImageService, ProductImageService>(opt =>
+{
+    opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
+}).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
+builder.Services.AddHttpClient<IProductDetailService, ProductDetailService>(opt =>
+{
+    opt.BaseAddress = new Uri($"{values.OcelotUrl}/{values.Catalog.Path}");
+}).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
 
 var app = builder.Build();
 
--- Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -2,55 +2,22 @@
 using MongoDB.Driver.Core.WireProtocol.Messages;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using MultiShop.WebUI.Services.CatalogServices.ProductServices;
 using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
 {
     public class _ProductListComponentPartial : ViewComponent
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public _ProductListComponentPartial(IHttpClientFactory httpClientFactory)
+        private readonly IProductService _productService;
+        public _ProductListComponentPartial(IProductService productService)
         {
-            _httpClientFactory = httpClientFactory;
+            _productService = productService;
         }
-
         public async Task<IViewComponentResult> InvokeAsync(string id)
         {
-            //id = "680e18dce50edeb2417e892f";
-            if (id == null)
-            {
-                var client = _httpClientFactory.CreateClient();
-                var responseMessage = await client.GetAsync("https://localhost:7070/api/Products");
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                    var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
-                    return View(values);
-                }
-                return View();
-            }
-            else
-            {
-
-                var client2= _httpClientFactory.CreateClient();
-                var responseMessage2 = await client2.GetAsync("https://localhost:7070/api/Categories/"+id);
-                var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
-                var values2 = JsonConvert.DeserializeObject<ResultCategoryDto>(jsonData2);
-
-
-                var client = _httpClientFactory.CreateClient();
-                var responseMessage = await client.GetAsync("https://localhost:7070/api/Products/ProductListWithCategoryByCategoryId/" + id);
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                    var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
-                    ViewBag.ct = values.Count >0 ? values2.CategoryName + " "+ "Kategorideki Ürünler" : values2.CategoryName + " " + "Kategorisinde Ürün Bulunmamaktadır";
-                    return View(values);
-                }
-               
-            }
-            return View();
+            var values = await _productService.GetProductsWithCategoryByCatetegoryIdAsync(id);
+            return View(values);
         }
     }
 }
