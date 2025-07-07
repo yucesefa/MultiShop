299987bbd2095d81c180aedaa9460cbd19f3931a
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-07-07 16:32:44 +0300

    postgres connection for message microservice

---
+++ Services/Message/MultiShop.Message/DAL/Context/MessageContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Entities;
+
+namespace MultiShop.Message.DAL.Context
+{
+    public class MessageContext : DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<UserMessage> UserMessages { get; set; }
+    }
+}
+
