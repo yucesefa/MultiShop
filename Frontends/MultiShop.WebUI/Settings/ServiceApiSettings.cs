a236c5f9afb0f789f158a2de5a08818a7395a8e0
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-07-07 17:58:40 +0300

    inbox and sendbox completed

--- Frontends/MultiShop.WebUI/Settings/ServiceApiSettings.cs
+++ Frontends/MultiShop.WebUI/Settings/ServiceApiSettings.cs
@@ -12,7 +12,7 @@
         public ServiceApi Basket { get; set; }
         public ServiceApi Payment { get; set; }
         public ServiceApi Comment { get; set; }
-        ////public ServiceApi Message { get; set; }
+        public ServiceApi Message { get; set; }
 
         public class ServiceApi
         {
