9f247d3f81d1220a35d40bcfb30939599f30b0a3
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-07-08 16:20:18 +0300

    catalog comment message statistics api added

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -11,6 +11,7 @@ using MultiShop.Catalog.Services.ProductDetailServices;
 using MultiShop.Catalog.Services.ProductImageServices;
 using MultiShop.Catalog.Services.ProductServices;
 using MultiShop.Catalog.Services.SpecialOfferServices;
+using MultiShop.Catalog.Services.StatisticServices;
 using MultiShop.Catalog.Settings;
 using System.Reflection;
 
@@ -23,6 +24,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     options.RequireHttpsMetadata = false;
 });
 // Add services to the container.
+builder.Services.AddScoped<IStatisticService, StatisticService>();
 builder.Services.AddScoped<ICategoryService,CategoryService>();
 builder.Services.AddScoped<IProductService,ProductService>();
 builder.Services.AddScoped<IProductDetailService,ProductDetailService>();
---
+++ Services/Message/MultiShop.Message/Controllers/UserMessageStatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Message.Services;
+
+namespace MultiShop.Message.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserMessageStatisticsController : ControllerBase
+    {
+        private readonly IUserMessageService _userMessageService;
+        public UserMessageStatisticsController(IUserMessageService userMessageService)
+        {
+            _userMessageService = userMessageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTotalMessageCount()
+        {
+            int messageCount = await _userMessageService.GetTotalMessageCount();
+            return Ok(messageCount);
+        }
+    }
+}
