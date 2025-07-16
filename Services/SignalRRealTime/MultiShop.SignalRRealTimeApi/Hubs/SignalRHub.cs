f03c940f60d6cda83f570679a83818b75fb0bfd0
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2025-07-16 19:43:33 +0300

    Retrieving instant comment information using signalr

---
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Hubs/SignalRHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MultiShop.SignalRRealTimeApi.Services.SignalRCommentServices;
+
+namespace MultiShop.SignalRRealTimeApi.Hubs
+{
+    public class SignalRHub:Hub
+    {
+        private readonly ISignalRCommentService _signalRCommentService;
+        // private readonly ISignalRMessageService _signalRMessageService;
+        public SignalRHub(ISignalRCommentService signalRCommentService)
+        {
+            _signalRCommentService = signalRCommentService;
+        }
+
+        public async Task SendStatisticCount()
+        {
+            var getTotalCommentCount = await _signalRCommentService.GetTotalCommentCount();
+            await Clients.All.SendAsync("ReceiveCommentCount", getTotalCommentCount);
+
+        }
+    }
+}
